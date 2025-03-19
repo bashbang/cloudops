7bb11b4cd4fb9eb04c42f64571c1cd715bdbc32a
Chris <chris@bashbang.com>
2025-03-19 16:43:21 -0700

    updated sysdig TF code to work with notification_channel creation

--- tf-sysdig/notification_channel.tf
+++ tf-sysdig/notification_channel.tf
@@ -1,9 +1,10 @@
-# # Define a notification channel
-# resource "sysdig_monitor_notification_channel_email" "tf_sre" {
-#     name                    = "TF SRE"
-#     recipients              = ["chris@bashbang.com"]
-#     enabled                 = true
-#     notify_when_ok          = true
-#     notify_when_resolved    = true
-#     send_test_notification  = true
-# }
+# Define a notification channel
+resource "sysdig_monitor_notification_channel_email" "tf_sre" {
+    name                    = "TF SRE"
+    enabled                 = true
+    recipients              = ["chris@bashbang.com"]
+    notify_when_ok          = true
+    notify_when_resolved    = true
+    send_test_notification  = true
+    share_with_current_team = true  # IMPORTANT - Needed to share with the current team only. Default would be all teams which results in a 403 error
+}
